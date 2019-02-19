c19841cf2c76ebcb984dce3e95e4f96e87e2dc84
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-02-18 22:12:49 -0500

    created list of items at store

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+item1 = {:name => "Carrots", :price => "1.00", :number_of_items => 20}
+item2 = {:name => "Apples", :price => "1.23", :number_of_items => 15}
+item3 = {:name => "Oranges", :price => "1.50", :number_of_items => 23}
+
+p "#{item1[:name]} are on sale for $#{item1[:price]}. We have #{item1[:number_of_items]} in stock."
+p "#{item2[:name]} are on sale for $#{item2[:price]}. We have #{item2[:number_of_items]} in stock."
+p "#{item3[:name]} are on sale for $#{item3[:price]}. We have #{item3[:number_of_items]} in stock."
