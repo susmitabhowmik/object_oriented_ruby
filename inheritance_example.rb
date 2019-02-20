95cc7df2bd9b99c269facd98f3a248d6ba20384f
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-02-19 21:55:33 -0500

    Example of inheritance

---
+++ inheritance_example.rb
@@ -0,0 +1,37 @@
+class Controls
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+
+
+class Car < Controls
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Controls
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+car.honk_horn
+bicycle = Bike.new
+bicycle.ring_bell
